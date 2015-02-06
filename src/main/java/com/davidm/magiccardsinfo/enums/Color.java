ef005d8ef136bec25b0251c262d0cab95edd241e
davidmotson <davidmotson@gmail.com>
2015-02-05 22:51:55 -0800

    removed warnings

--- src/main/java/com/davidm/magiccardsinfo/enums/Color.java
+++ src/main/java/com/davidm/magiccardsinfo/enums/Color.java
@@ -1,8 +1,6 @@
 package com.davidm.magiccardsinfo.enums;
 
 import java.util.ArrayList;
-import java.util.EnumSet;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
